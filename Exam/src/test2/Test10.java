50a0fc059bf4842d255cb6d7a857bb00b92af2d4
gahui446 <gahui446@gmail.com>
2023-12-29 14:45:33 +0900

    Exam commit

---
+++ Exam/src/test2/Test10.java
@@ -0,0 +1,20 @@
+package test2;
+/* 
+ * 날짜: 2023-12-29
+ * 이름: 이가희
+ * 내용 : 자바 피보나치 수열 재귀 메서드 연습문제
+ */
+public class Test10 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) {
+			System.out.print(fibo(i)+" ");
+		}
+	}
+	public static int fibo(int n) {
+		if(n <=1) {
+			return n;
+		}
+		return fibo(n-2)+fibo(n-1);
+	}
+}
