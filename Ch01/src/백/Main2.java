befd45a02980c73fa45444ba060689fa17e22c16
gahui446 <gahui446@gmail.com>
2023-12-29 16:08:59 +0900

    Ch01 커밋

--- Ch01/src/백/Main2.java
+++ Ch01/src/백/Main2.java
@@ -1,9 +1,28 @@
 package 백;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main2 {
-
-	public static void main(String[] args) {
-
+	static Set<String> set = new HashSet<>();
+	static StringBuffer sb = new StringBuffer();
+	static String a;
+	static int k;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		a= br.readLine();
+		k=a.length();
 	}
-
+	public static void methodA() {
+		int p=1;
+		while(p<=k) {
+			for(int i=p ;i<a.length(); i++) {
+				
+			}
+			p++;
+		}
+	}
 }
