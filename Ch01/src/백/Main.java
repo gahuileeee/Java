b665cfe0c7b798ad76abf6d4cd5183866b94e71f
gahui446 <gahui446@gmail.com>
2024-01-02 14:51:54 +0900

    커밋

--- Ch01/src/백/Main.java
+++ Ch01/src/백/Main.java
@@ -3,55 +3,16 @@ package 백;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int [] brr;
-	static StringBuffer sb = new StringBuffer();
-	static int [] arr;
+	static int V;
+	static int E;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int [M];
-		brr = new int [N];
-		st= new StringTokenizer(br.readLine());
-		for(int i=0 ; i<N; i++)
-		{
-			brr[i]=Integer.parseInt(st.nextToken());
-		} //입력 다 받음
-		Arrays.sort(brr);
-		methodA(0);
-		System.out.println(sb.toString());
-		
-	}
-	public static void methodA(int i) {
-		int j;
-		if(i==M) {
-			for(i=0; i<M; i++) {
-				sb.append(arr[i]+" ");
-			}
-			sb.append("\n");
-			return;
-		}
 	
-		for(j=0; j<N; j++) {
-			arr[i]=brr[j];
-			if(promising(i, j)) {
-				methodA(i+1);
-			}
-		}
-	} // methodA 종료
-	
-	public static boolean promising(int i, int j) {
-		if( i==0) return true;
-		else if(arr[i-1]<brr[j]) return true;
-		else return false;
 	}
 }
