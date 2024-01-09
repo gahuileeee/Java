95bcade5512b300045547df82b4616b205dc9845
gahui446 <gahui446@gmail.com>
2024-01-09 17:48:50 +0900

    commit

--- Ch01/src/백/Main2.java
+++ Ch01/src/백/Main2.java
@@ -1,7 +1,5 @@
 package 백;
-/*
- * 평범한 배낭 12865
- */
+//	마인크래프트 18111
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,36 +7,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
-class xy {
-	int x;
-	int y;
-	xy(int x, int y){
-		this.x=x;
-		this.y=y;
-	}
-}
 public class Main2 {
 	static int N;
-	static int K;
-	static xy arr [];
-	static int sum [];
+	static int M;
+	static int B;
+	static int map [][] ;
 	static StringBuffer sb = new StringBuffer();
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N= Integer.parseInt(st.nextToken());
-		K= Integer.parseInt(st.nextToken());
-		arr = new xy [N];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a =Integer.parseInt(st.nextToken());
-			int b =Integer.parseInt(st.nextToken());
-			arr[i]=new xy(a,b);
-		}
-	
-	}
-	
-	public static void dp() {
+			st= new StringTokenizer(br.readLine());
+			for(int k=0; k<M; k++) {
+				map[i][k]=Integer.parseInt(st.nextToken());
+			}
+		}//map 입력 받음.
+		
 		
 	}
 }
