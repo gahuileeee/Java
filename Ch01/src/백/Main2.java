8334d6652d9574dacdd3b396ea0ff682f2ec34b5
gahui446 <gahui446@gmail.com>
2024-01-11 17:50:49 +0900

    commit

--- Ch01/src/백/Main2.java
+++ Ch01/src/백/Main2.java
@@ -1,65 +1,63 @@
 package 백;
-// 용액
-
+// 고냥이 16472
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main2 {
-	static int N;
-	static int [] arr;
-	static int tiny;
-	static int [] a;
+
 	static StringBuffer sb = new StringBuffer();
+	static int N;
+	static String A;
+//	static ArrayList<Character> list = new ArrayList<>();
+	static Map<Character, Integer> map = new HashMap<>();
+	static int count;
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N= Integer.parseInt(br.readLine());
-		arr= new int[N];
-		a = new int[2];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
+		if(N==1) {
+			System.out.println(1);
+			System.exit(0);
 		}
-		tiny = Integer.MAX_VALUE;
-		search();
-		System.out.println(a[0]+" "+a[1]);
-		arr[0]=0;
-		arr[1]=0;
+		A = br.readLine();
+		count=0;
 		two();
-		System.out.println(a[0]+" "+a[1]);
-	}
-	public static void search() {
-		for(int i=0; i<N-1; i++) {
-			for(int k=i+1; k<N; k++) {
-				int c= Math.abs(arr[i]+arr[k]);
-				if(c<tiny) {
-					tiny=c;
-					a[0]=arr[i];
-					a[1]=arr[k];
-				}
-			}
-		}
+		System.out.println(count);
 	}
 	public static void two() {
-		int start = 0;
-		int end = N-1;
-		
-		while(start<=end) {
-			int c= arr[start]+arr[end];
-			if(Math.abs(c)<tiny) {
-				tiny=c;
-				a[0]=arr[start];
-				a[0]=arr[end];
+		int start=0;
+		int end=1;
+		map.put(A.charAt(0),0);
+		map.put(A.charAt(1),0);
+		String B="";
+		B+=A.charAt(0)+""+A.charAt(1);
+		end++;
+		while(end<=A.length()) {
+			map.put(A.charAt(end), 0);
+			if(map.size()<=N) {
+				B+=A.charAt(end+1)+"";
+				end++;
+			}else {
+				if(B.length()>count) {
+					count=B.length();
+				}
+				char c= B.charAt(0);
+				map.remove(c);
+				while(true) {
+					if(B.charAt(0)==c) {
+						B=B.substring(1, B.length()-1);
+					}else {
+						break;
+					}
+				}
+				end++;
 			}
-			if(c<0) {
-				start++;
-			}else if(c>0){
-				end--;
-			}if(c==0) {
-				break;
-			}
+			
 		}
+		
 	}
 }
